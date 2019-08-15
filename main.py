e529a6d7141b1c5aa628b2e38e0593ab2a9b143a
Masao Kitamura <masaok@gmail.com>
2019-08-15 11:52:02 -0700

    Copied from hogwarts demo

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import webapp2
-
+import os
+import jinja2
+from models import Student, Wand, House, Course, Enrollment, Teacher
+from seed import seed_data
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+class LoadDataHandler(webapp2.RequestHandler):
+    def get(self):
+        seed_data()
+        self.response.write('Thank you. The Datastore is now seeded.')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/seed', LoadDataHandler)
 ], debug=True)
